5ebc4c1305ad8d95c2b0c3fa5edde643058b87c7
cd168168 <158546685+cd168168@users.noreply.github.com>
2024-05-20 15:18:25 +0800

    Update streamlit_account.py

--- streamlit_account.py
+++ streamlit_account.py
@@ -67,10 +67,11 @@ def query_profit():
             
         api.logout()
 
-    for data in pnlList:
-        st.write(data)
+    # for data in pnlList:
+    #     st.write(data)
 
-    #df = pd.DataFrame([nameList,pnlList],columns=["name","pnl"])
+    df = pd.DataFrame([nameList,pnlList],columns=["name","pnl"])
+    st.table(df)
     #st.dataframe(df,hide_index=True)
     
     # df = pd.DataFrame(
