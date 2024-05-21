af66c9d1ace779592a8e116189676b5344e2f62a
cd168168 <158546685+cd168168@users.noreply.github.com>
2024-05-21 15:04:20 +0800

    Update streamlit_account.py

--- streamlit_account.py
+++ streamlit_account.py
@@ -75,8 +75,6 @@ def query_profit():
         {
             "name": ["Roadmap", "Extras", "Issues"],
             "url": ["https://roadmap.streamlit.app", "https://extras.streamlit.app", "https://issues.streamlit.app"],
-            "stars": [random.randint(0, 1000) for _ in range(3)],
-            "views_history": [[random.randint(0, 5000) for _ in range(30)] for _ in range(3)],
         }
     )
     
@@ -84,15 +82,7 @@ def query_profit():
         df,
         column_config={
             "name": "App name",
-            "stars": st.column_config.NumberColumn(
-                "Github Stars",
-                help="Number of stars on GitHub",
-                format="%d ⭐",
-            ),
             "url": st.column_config.LinkColumn("App URL"),
-            "views_history": st.column_config.LineChartColumn(
-                "Views (past 30 days)", y_min=0, y_max=5000
-            ),
         },
         hide_index=True,
     )
