b7f8431546e88692ba66ed046f9fc1e13e0c16fd
cd168168 <158546685+cd168168@users.noreply.github.com>
2024-09-24 15:39:59 +0800

    Update streamlit_account.py

--- streamlit_account.py
+++ streamlit_account.py
@@ -42,11 +42,8 @@ def query_profit():
 
         nameList.append(value[0])
 	    
-        if profitloss.equity_amount-value[1]>0:
-            profitList.append(profitloss.equity_amount-value[1])
-        else:
-            profitList.append(0)
-		
+        profitList.append(profitloss.equity_amount-value[1])
+        
         if key=="edward":
             receiveList.append(0)
         else:
